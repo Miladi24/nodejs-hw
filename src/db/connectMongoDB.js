f5612a4e4139f8c212a40a8c3010e6aa4e2f6ab5
Mila <miladiachenko2404@gmail.com>
2026-01-17 15:31:33 +0200

    MongoDB integration

---
+++ src/db/connectMongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ MongoDB connection established successfully");
+  } catch(error) {
+    console.error("Error conections to MongoDB: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectMongoDB;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
-// src/server.js
-
-import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
 import pino from 'pino-http';
+import cors from 'cors';
+import helmet from "helmet";
+import 'dotenv/config';
+import connectMongoDB from './db/connectMongoDB.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 app.use(
   pino({
     level: 'info',
@@ -19,47 +22,22 @@ app.use(
         colorize: true,
         translateTime: 'HH:MM:ss',
         ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
   }),
 );
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
-});
+const PORT = process.env.PORT ?? 3000;
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(notesRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
+app.use(notFoundHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+app.use(errorHandler);
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: err.message,
-  });
-});
-
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
