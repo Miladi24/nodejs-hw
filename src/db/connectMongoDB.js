41ccf7836aa94b183ec62e189528663ffad52dce
Mila <miladiachenko2404@gmail.com>
2026-02-03 19:53:32 +0200

    comit

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,11 +1,18 @@
-import mongoose from "mongoose";
+
+import mongoose from 'mongoose';
+import { Note } from '../models/note.js';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("✅ MongoDB connection established successfully");
-  } catch(error) {
-    console.error("Error connecting to MongoDB: ", error);
+    const mongoUrl = process.env.MONGO_URL;
+    await mongoose.connect(mongoUrl);
+    console.log('✅ MongoDB connection established successfully');
+
+    // синохронізація індексів для моделі Note
+    await Note.syncIndexes();
+    console.log('Indexes synced successfully');
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB:', error.message);
     process.exit(1);
   }
 };
