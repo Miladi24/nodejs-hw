d460722ec9a0d423c75e610eac7c96889d02d836
Mila <miladiachenko2404@gmail.com>
2026-02-11 00:12:49 +0200

    mail

--- src/server.js
+++ src/server.js
@@ -1,32 +1,31 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import 'dotenv/config';
-import { errors } from 'celebrate';
 
-import { logger } from './middleware/logger.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
-import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger);
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
-
-const PORT = process.env.PORT ?? 3000;
 
 app.use(authRoutes);
 app.use(notesRoutes);
-
-app.use(errors());
+app.use(userRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
